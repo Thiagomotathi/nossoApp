9d2f3e3c2098fba4a8ee2ec6a9ce11b586e4a164
Thiago_mota _program <128191337+Thiagomotathi@users.noreply.github.com>
2024-04-25 13:46:49 -0300

    Initial Commit

---
+++ nossoApp/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  nossoApp
+//
+//  Created by User on 25/04/24.
+//
+
+import SwiftUI
+
+struct Integrantes: Identifiable {
+    
+    let name: String
+    var id: String{name}
+}
+
+let integrantes = [
+  Integrantes(name: "🐲  Rafa "),
+  Integrantes(name: "💚  Thigo "),
+  Integrantes(name: "🌟  Clara "),
+  Integrantes(name: "🧙‍♂️  Dan "),
+  Integrantes(name: "💯  Todes! ")
+  
+]
+
+struct ContentView: View {
+    @State private var selectedIntegrante: String = "nenhum" // Default selection
+    var body: some View {
+        
+        NavigationView{
+            
+            List {
+                Picker("Integrante favorito", selection: $selectedIntegrante) {
+                    ForEach(integrantes) { integrante in
+                        Text(integrante.name)
+                            .tag(integrante.name)// Set tag for selection
+                    }
+                }
+                .pickerStyle(WheelPickerStyle())
+                Text("Você selecionou: \(selectedIntegrante)") // Display selected integrante
+            }
+            .navigationTitle("Integrantes")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nossoApp/nossoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  nossoAppApp.swift
+//  nossoApp
+//
+//  Created by User on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct nossoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
